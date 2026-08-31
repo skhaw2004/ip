f078d03562243040d7d62e1054eb9f0e3654fdb4
skhaw2004 <stuartkhaw@gmail.com>
2026-08-31 14:02:29 +0800

    Add JavaDoc comment for containsKeyword method

--- src/main/java/stuart/task/Task.java
+++ src/main/java/stuart/task/Task.java
@@ -94,6 +94,13 @@ public class Task {
         return false;
     }
 
+    /**
+     * Checks whether this task contains the keyword inputted.
+     * Works for all instances of Task as only requires the task description
+     *
+     * @param keyword
+     * @return true if this task contains the keyword
+     */
     public boolean containsKeyword(String keyword) {
         return this.description.contains(keyword);
     }
