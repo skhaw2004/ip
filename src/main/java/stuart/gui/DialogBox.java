1f5d08e962f380c62a887dcfa8739927a2a428ed
skhaw2004 <stuartkhaw@gmail.com>
2026-09-01 22:09:03 +0800

    Refactor GUI with FXML

--- src/main/java/stuart/gui/DialogBox.java
+++ src/main/java/stuart/gui/DialogBox.java
@@ -1,7 +1,12 @@
 package stuart.gui;
 
+import java.io.IOException;
+import java.util.Collections;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -10,24 +15,29 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 /**
- * A chat bubble: a speaker's avatar next to their message text. Used for
- * both the user's messages and Stuart's replies, mirrored left/right via
- * {@link #getUserDialog} and {@link #getStuartDialog}.
+ * A chat bubble: a speaker's avatar next to their message text, defined in
+ * {@code /view/DialogBox.fxml}. Used for both the user's messages and
+ * Stuart's replies, mirrored left/right via {@link #getUserDialog} and
+ * {@link #getStuartDialog}.
  */
 public class DialogBox extends HBox {
-    private final Label text;
-    private final ImageView displayPicture;
+    @FXML
+    private Label dialog;
+    @FXML
+    private ImageView displayPicture;
 
-    private DialogBox(String s, Image i) {
-        text = new Label(s);
-        text.setWrapText(true);
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not load /view/DialogBox.fxml", e);
+        }
 
-        displayPicture = new ImageView(i);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
-
-        this.setAlignment(Pos.TOP_RIGHT);
-        this.getChildren().addAll(text, displayPicture);
+        dialog.setText(text);
+        displayPicture.setImage(img);
     }
 
     /**
@@ -35,32 +45,32 @@ public class DialogBox extends HBox {
      * the right, to visually distinguish Stuart's replies from user input.
      */
     private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
-        this.getChildren().setAll(tmp);
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
     }
 
     /**
      * Creates a dialog box for a message the user sent.
      *
-     * @param s the message text
-     * @param i the user's avatar
+     * @param text the message text
+     * @param img the user's avatar
      * @return the dialog box, with the avatar on the right
      */
-    public static DialogBox getUserDialog(String s, Image i) {
-        return new DialogBox(s, i);
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
     }
 
     /**
      * Creates a dialog box for one of Stuart's replies.
      *
-     * @param s the reply text
-     * @param i Stuart's avatar
+     * @param text the reply text
+     * @param img Stuart's avatar
      * @return the dialog box, mirrored so the avatar is on the left
      */
-    public static DialogBox getStuartDialog(String s, Image i) {
-        DialogBox db = new DialogBox(s, i);
+    public static DialogBox getStuartDialog(String text, Image img) {
+        DialogBox db = new DialogBox(text, img);
         db.flip();
         return db;
     }
